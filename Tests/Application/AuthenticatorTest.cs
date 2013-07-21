d344e0146cae874748a2ba2a86507d22f651093d
Adam Gooch <agooch@8thlight.com>
2013-07-21 18:02:42 -0400

    Cleanup authenticator tests using statements

--- Tests/Application/AuthenticatorTest.cs
+++ Tests/Application/AuthenticatorTest.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.Specialized;
-using System.Linq;
 using System.Web;
-using Moq;
 using NUnit.Framework;
 using Application;
 
